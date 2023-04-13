05a6f525e14424b54af991257b599e6b783b69b8
doyoung <doyoung9508@126.com>
2023-04-13 20:06:46 +0800

    add

--- src/user-prompting.js
+++ src/user-prompting.js
@@ -2,14 +2,42 @@ const hx = require('hbuilderx');
 
 //该方法将在插件激活的时候调用
 function activate(context) {
-    context.subscriptions.push(hx.commands.registerCommand('add.custom.prompting',async (param) => {
+    context.subscriptions.push(hx.commands.registerCommand('add.custom.prompting', async (param) => {
         function getFormItems(options) {
             const dialogTitle = ""
             const dialogFooter = ""
             return {
                 title: dialogTitle,
                 footer: dialogFooter,
-                formItems: []
+                formItems: [{
+                        type: "input",
+                        name: "projectName",
+                        label: "普通输入框",
+                        placeholder: "这是一个普通输入框",
+                        value: "",
+                        disabled: false
+                    },
+                    {
+                        type: "comboBox",
+                        name: "functionNameInput",
+                        items: ["item1", "item2", "item3"],
+                        index: 0
+                    },
+                    {
+                        type: "checkBox",
+                        name: "checkBox1",
+                        label: "复选框",
+                        value: false
+                    },
+                    {
+                        type: "input",
+                        name: "projectName1",
+                        label: "普通输入框",
+                        placeholder: "这是一个普通输入框",
+                        value: "",
+                        disabled: false
+                    }
+                ]
             }
         }
         const btnCancelText = "取消(&C)"
@@ -17,8 +45,8 @@ function activate(context) {
 
         const res = await hx.window.showFormDialog({
             ...getFormItems(),
-            width: 700,
-            height: 550,
+            width: 400,
+            height: 300,
             customButtons: [{
                 text: btnCancelText
             }, {
