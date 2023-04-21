b1c691ae01ab01d62aa3512f16473e4f01d417ed
doyoung <doyoung9508@126.com>
2023-04-21 11:05:35 +0800

    // 更新接口

--- src/prompting_content_provider.js
+++ src/prompting_content_provider.js
@@ -5,34 +5,52 @@ const hx = require('hbuilderx');
 const axios = require("axios").default;
 
 function getImageTag(filePath) {
-	return `<img src="${filePath}" width="300", height="500">`;
+    return `<img src="${filePath}" width="300", height="500">`;
 }
 
 function getRandomButtoms() {
-	return {
-		"摸鱼日历": () => {
-			axios.get("https://api.vvhan.com/api/moyu", {
-					responseType: 'arraybuffer'
-				})
-				.then(res => {
-					const moyu = path.join(os.tmpdir(), "moyu.png");
-					fs.writeFileSync(moyu, res.data);
-					hx.window.showInformationMessage(getImageTag(moyu));
-				})
-		},
-		"来一张写真": () => {
-			axios.get("https://api.vvhan.com/api/acgimg", {
-					responseType: 'arraybuffer'
-				})
-				.then(res => {
-					const moyu = path.join(os.tmpdir(), "girl.png");
-					fs.writeFileSync(moyu, res.data);
-					hx.window.showInformationMessage(getImageTag(moyu));
-				})
-		}
-	}
+    return {
+        "微博热搜": () => {
+            axios.get("https://api.vvhan.com/api/hotlist?type=wbHot")
+                .then(res => {
+                    const data = res.data;
+                    if (data.success) {
+                        let text = "";
+                        for (let i = 0; i < data.data.length && i < 10; ++i) {
+                            const one = data.data[i];
+                            text += `<p>标题：${one.title}<br>热度：${one.hot}    <a href='${one.url}'>详情</a></p>`;
+                        }
+                        hx.window.showInformationMessage(text);
+                    }
+                })
+                .catch(err => {
+                    hx.window.setStatusBarMessage('获取热搜榜失败...', 2000, 'error');
+                })
+        },
+        "摸鱼日历": () => {
+            axios.get("https://api.vvhan.com/api/moyu", {
+                    responseType: 'arraybuffer'
+                })
+                .then(res => {
+                    const moyu = path.join(os.tmpdir(), "moyu.png");
+                    fs.writeFileSync(moyu, res.data);
+                    hx.window.showInformationMessage(getImageTag(moyu));
+                })
+        }
+        /* ,
+        		"来一张写真": () => {
+        			axios.get("/Users/doyoung/MyProject/timing-prompting", {
+        					responseType: 'arraybuffer'
+        				})
+        				.then(res => {
+        					const moyu = path.join(os.tmpdir(), "girl.png");
+        					fs.writeFileSync(moyu, res.data);
+        					hx.window.showInformationMessage(getImageTag(moyu));
+        				})
+        		} */
+    }
 }
 
 module.exports = {
-	getRandomButtoms
+    getRandomButtoms
 }
